79fe815ed710d94f970c22aad0520fe7516f104d
STRIKER013 <66685038+STRIKER013@users.noreply.github.com>
2023-09-18 16:05:00 -0500

    Edit - I make a news variables with float, double, decimal and string and write in the console

--- CourseNetCore/Program.cs
+++ CourseNetCore/Program.cs
@@ -1,7 +1,16 @@
 ﻿int year = 2023;
+float since = 4.2F;
+double age = 21.9;
+decimal semester = 9.05M;
+String carrer = "Enginner of System";
+String name = "Omar Parra";
+
 
 Console.WriteLine("Hello, World!");
 
-Console.WriteLine("I'm Omar Parra, today is "+year);
+Console.WriteLine("I'm " + name + ", today is {0} and i'm {1} years old, i'm study {2} in the University Popular of Cesar " +
+"i stay in {3} semester but i'm learnig to programation since {4} semester", year, age, carrer,semester, since);
+
+
 
 Console.ReadLine(); 
