97319ac99f0bad76341f8a67846777c43b4cbb58
STRIKER013 <66685038+STRIKER013@users.noreply.github.com>
2023-09-20 12:34:59 -0500

    Add new varibles, bool, char and one data storage structure

--- CourseNetCore/Program.cs
+++ CourseNetCore/Program.cs
@@ -4,7 +4,11 @@ double age = 21.9;
 decimal semester = 9.05M;
 String carrer = "Enginner of System";
 String name = "Omar Parra";
-String ty = " thanks!!!";
+String ty = "thanks!!!";
+bool verification = true;
+char pin = '*';
+Decimal structDecimal = 99.999M;
+
 
 
 Console.WriteLine("Hello, World!");
@@ -12,7 +16,9 @@ Console.WriteLine("Hello, World!");
 Console.WriteLine("I'm " + name + ", today is {0} and i'm {1} years old, i'm study {2} in the University Popular of Cesar " +
 "i stay in {3} semester but i'm learnig to programation since {4} semester", year, age, carrer,semester, since);
 
-Console.WriteLine(ty);
+Console.WriteLine("All this information about me is: "+ verification +  structDecimal+ "%");
+
+Console.WriteLine(pin + ty + pin);
 
 
 Console.ReadLine(); 
