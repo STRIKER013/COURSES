624956950ea0ba8902452bc0d99f13f41801063b
STRIKER013 <66685038+STRIKER013@users.noreply.github.com>
2023-09-20 14:12:17 -0500

    Add new project about how do i use basic operactors

--- CourseNetCore/Program.cs
+++ CourseNetCore/Program.cs
@@ -10,15 +10,14 @@ char pin = '*';
 Decimal structDecimal = 99.999M;
 
 
-
 Console.WriteLine("Hello, World!");
 
 Console.WriteLine("I'm " + name + ", today is {0} and i'm {1} years old, i'm study {2} in the University Popular of Cesar " +
 "i stay in {3} semester but i'm learnig to programation since {4} semester", year, age, carrer,semester, since);
 
-Console.WriteLine("All this information about me is: "+ verification +  structDecimal+ "%");
+Console.WriteLine("All this information about me is: \n" + verification + structDecimal + "%");
 
 Console.WriteLine(pin + ty + pin);
 
 
-Console.ReadLine(); 
+Console.ReadLine();
