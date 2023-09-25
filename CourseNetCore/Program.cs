6b943d7b92fbf1b31e7e6a487deb09420d4f289d
STRIKER013 <66685038+STRIKER013@users.noreply.github.com>
2023-09-25 10:15:12 -0500

    Add CourseNetCoreOperators and use diferent operator

--- CourseNetCore/Program.cs
+++ CourseNetCore/Program.cs
@@ -2,7 +2,7 @@
 float since = 4.2F;
 double age = 21.9;
 decimal semester = 9.05M;
-String carrer = "Enginner of System";
+String carrer = "System Engineer";
 String name = "Omar Parra";
 String ty = "thanks!!!";
 bool verification = true;
@@ -12,7 +12,7 @@ Decimal structDecimal = 99.999M;
 
 Console.WriteLine("Hello, World!");
 
-Console.WriteLine("I'm " + name + ", today is {0} and i'm {1} years old, i'm study {2} in the University Popular of Cesar " +
+Console.WriteLine("I'm " + name + ", today is {0} and i'm {1} years old, i'm studing {2} at the University Popular of Cesar " +
 "i stay in {3} semester but i'm learnig to programation since {4} semester", year, age, carrer,semester, since);
 
 Console.WriteLine("All this information about me is: \n" + verification + structDecimal + "%");
